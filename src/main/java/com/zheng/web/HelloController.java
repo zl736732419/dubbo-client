95a6bdefe06672fd39647c06dbda90e2bdada448
zhenglian <zhenglian@192.168.56.1>
2016-08-16 22:55:17 +0800

    添加dubbo参数验证

--- src/main/java/com/zheng/web/HelloController.java
+++ src/main/java/com/zheng/web/HelloController.java
@@ -4,17 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.zheng.domain.Person;
 import com.zheng.service.HelloService;
+import com.zheng.service.ValidationService;
 
 @Controller
 public class HelloController {
 	@Autowired
 	private HelloService helloService;
 	
+	@Autowired
+	private ValidationService validationService;
+	
 	@RequestMapping("/hello")
 	public void sayHello() {
 		String hello = helloService.sayHello("world");
 		System.out.println(hello);
+	}
+	
+	@RequestMapping("/validation")
+	public void validation() {
+		Person person = new Person("zl", 18);
+		String desc = validationService.descs(person);
+		System.out.println(desc);
+		
+		validationService.delete(1);
 	}
 	
 }
