b03ac4012a5d48595f13738220a99dfa65d17613
zhenglian <zhenglian@192.168.56.1>
2016-08-20 10:00:29 +0800

    添加回声测试

--- src/test/java/com/zheng/test/ConsumerTest.java
+++ src/test/java/com/zheng/test/ConsumerTest.java
@@ -3,6 +3,7 @@ package com.zheng.test;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.alibaba.dubbo.rpc.service.EchoService;
 import com.zheng.service.HelloService;
 
 public class ConsumerTest {
@@ -10,11 +11,24 @@ public class ConsumerTest {
 	@Test
 	@SuppressWarnings("resource")
 	public void consume() throws Exception {
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(new String[] { "consumer.xml" });
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+				new String[] {"spring-mvc.xml", "applicationContext.xml"});
 		ctx.start();
 		HelloService helloService = (HelloService) ctx.getBean("helloService");
 		String hello = helloService.sayHello("world");
-		
 		System.out.println(hello);
+		
+	}
+	
+	@Test
+	public void echoTest() throws Exception {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+				new String[] {"spring-mvc.xml", "applicationContext.xml"});
+		ctx.start();
+		HelloService helloService = (HelloService) ctx.getBean("helloService");
+		EchoService echoService = (EchoService) helloService;
+		
+		String resp = (String) echoService.$echo("hello");
+		System.out.println(resp);
 	}
 }
